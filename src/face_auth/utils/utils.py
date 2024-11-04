4ec92552ea3444ff4439826dfe34dbc379769978
chekist <georg4932@mail.ru>
2024-11-05 05:43:50 +1000

    refactore

--- src/face_auth/utils/utils.py
+++ src/face_auth/utils/utils.py
@@ -1,11 +1,15 @@
 import os
+import io
 
 import torch
+from torchvision import transforms
 
-from config import NOW_DATE
+from PIL import Image
+
+from src.face_auth.utils.config import NOW_DATE
 from src.config import BASE_DIR
 
-from model import model
+from src.face_auth.utils.model import model
 
 
 def save_model(model_name=f'{NOW_DATE}_model.pth'):
@@ -27,3 +31,36 @@ def bin_tens_to_hex(bin_tens: torch.Tensor) -> str:
     hex_line = ''.join(pad_hexs)
 
     return hex_line
+
+
+def predicate(content: bytes):
+    img = FaceImage(content)
+
+    load_model('best_model_BCEloss.pth')
+
+    with torch.no_grad():
+        model.eval()
+        predicated = torch.round(model(img.get().unsqueeze(0)))
+
+    return predicated
+
+
+class FaceImage:
+    def __init__(self, content: bytes):
+        self.content = content
+        self.transform = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+        ])
+
+    def get(self) -> Image:
+        image = Image.open(io.BytesIO(self.content))
+        image = self.transform(image).cuda()
+        return image
+
+
+# keys = torch.load(BASE_DIR / 'models/keys.pth', weights_only=True)
+# new_key = {f'{key}': hashlib.sha512(bytes.fromhex(bin_tens_to_hex(val))).hexdigest() for key, val in keys.items()}
+# print(new_key)
+# json.dump(new_key, open('some_keys.json', 'w', encoding='utf-8'), indent=4)
